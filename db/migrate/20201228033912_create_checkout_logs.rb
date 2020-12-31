9d1f3f85d48392da7f9d7312cfeee50d29443ce3
Kirk MacRae <kirk.macrae@gmail.com>
2020-12-30 23:21:21 -0500

    removed uppercase column names

--- db/migrate/20201228033912_create_checkout_logs.rb
+++ db/migrate/20201228033912_create_checkout_logs.rb
@@ -1,9 +1,9 @@
 class CreateCheckoutLogs < ActiveRecord::Migration[6.0]
   def change
     create_table :checkout_logs do |t|
-      t.datetime :CheckoutDate
-      t.datetime :DueDate
-      t.datetime :ReturnedDate
+      t.datetime :checkout_date
+      t.datetime :due_date
+      t.datetime :returned_date
       t.references :user, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
 
